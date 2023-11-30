7c43ebdf7c53a722abe5fcf37ae39f4a98df7605
javad bayat <39430951+javadbayat1365@users.noreply.github.com>
2023-11-30 14:14:14 +0330

    Add Repository-controller-actions-docker compose

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,84 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Entities.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly ILogger<CatalogController> _logger;
+        private readonly IProductsRepository _productsRepository;
+
+        public CatalogController(ILogger<CatalogController> logger, IProductsRepository productsRepository)
+        {
+            this._logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this._productsRepository = productsRepository ?? throw new ArgumentNullException(nameof(productsRepository)); ;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _productsRepository.GetProducts();
+            return Ok(products);
+        }
+
+
+        [HttpGet("{Id:length(24)}", Name = "GetProduct")]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult<Product>> GetProductById(string Id)
+        {
+            var product = await _productsRepository.GetProduct(Id);
+            if (product is null)
+            {
+                _logger.LogError($"product with id :{Id} not found!");
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+
+        [HttpGet]
+        [Route("[action]/categoryName", Name = "GetProductByCategory")]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> GetProductByCategoryName(string categoryName)
+        {
+            var products = await _productsRepository.GetProductByCategory(categoryName);
+            return Ok(products);
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            await _productsRepository.CreateProduct(product);
+
+            //Redirection to GetProduct action
+            return CreatedAtRoute("GetProduct", new { Id = product.Id }, product);
+        }
+
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> UpdateProduct(Product product)
+        {
+            var result =await _productsRepository.UpdateProduct(product);
+            return Ok(result);
+        }
+
+
+        [HttpDelete("{Id:length(24)}",Name ="DeleteProduct")]
+        [ProducesResponseType(typeof(Product),(int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> DeleteProductById(string Id)
+        {
+            var result = await _productsRepository.DeleteProduct(Id);
+            return Ok(result);
+        }
+
+    }
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,6 @@
+using Catalog.API.Data;
+using Catalog.API.Entities.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,15 +8,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(options => {
+    options.SwaggerDoc("v1",new Microsoft.OpenApi.Models.OpenApiInfo() { Title="Catalog.API", Version="V1" });
+});
+builder.Services.AddScoped<IProductsRepository, ProductsRepository>();
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json","Catalog.API V1");
+    });
 }
 
 app.UseAuthorization();
