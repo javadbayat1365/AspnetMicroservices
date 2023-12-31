8922da97544caaa6d32686a1305ca4476c5ca28a
javad bayat <39430951+javadbayat1365@users.noreply.github.com>
2023-12-31 21:51:33 +0330

    Add-ApplicationServiceRegistrationClass-In-ApplicationBehavior

---
+++ src/Services/Ordering.Application/Ordering.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+using System.Reflection;
+
+namespace Ordering.Application;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddMediatR(option => { 
+        option.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+        });
+
+        services.AddTransient(typeof(IPipelineBehavior<,>),typeof(UnhandleExceptionBehavior<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>),typeof(ValidationBehavior<,>));
+
+        return services;
+    }
+}
