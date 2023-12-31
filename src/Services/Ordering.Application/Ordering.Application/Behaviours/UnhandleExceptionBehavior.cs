bc8879b0942524938e65a0514f373daf4f9e6b4c
javad bayat <39430951+javadbayat1365@users.noreply.github.com>
2023-12-31 21:25:03 +0330

    add behavior MediarR => ExceptionHandler

---
+++ src/Services/Ordering.Application/Ordering.Application/Behaviours/UnhandleExceptionBehavior.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Ordering.Application.Behaviours;
+
+public class UnhandleExceptionBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+{
+	private readonly ILogger<TRequest> _logger;
+
+    public UnhandleExceptionBehavior(ILogger<TRequest> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+		try
+		{
+			return await next();
+		}
+		catch (Exception ex)
+		{
+			var name = typeof(TRequest).Name;
+			_logger.LogError(ex,$"Application Request: Unhandle Exception for request {name}");
+			throw;
+		}
+    }
+}
