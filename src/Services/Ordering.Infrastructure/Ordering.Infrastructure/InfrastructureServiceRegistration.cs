dc2957da5a895b50ef113573625e33b620cd8865
javad bayat <39430951+javadbayat1365@users.noreply.github.com>
2024-01-04 14:28:03 +0330

    Add-InfrastructureServiceRegistration-To-OrderingAPI-Layer

---
+++ src/Services/Ordering.Infrastructure/Ordering.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Contracts.Infrastructures;
+using Ordering.Application.Contracts.Persistances;
+using Ordering.Application.Models;
+using Ordering.Infrastructure.Email;
+using Ordering.Infrastructure.Persistence;
+using Ordering.Infrastructure.Repositories;
+
+namespace Ordering.Infrastructure;
+
+public static class InfrastructureServiceRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection  services,IConfiguration configuration)
+    {
+        services.AddDbContext<OrderContext>(options => {
+            options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString"));
+        });
+
+        services.AddScoped<IOrderRepository,OrderRepository>();
+        services.AddScoped(typeof(IAsyncRepository<>),typeof(RepositoryBase<>));
+
+        services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+        services.AddTransient<IEmailService, EmailService>();
+
+        return services;
+    }
+}
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,3 +1,6 @@
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
