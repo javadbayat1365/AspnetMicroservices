92ce11f467cad9587617fd6305ccbdea3bd756d8
javad bayat <39430951+javadbayat1365@users.noreply.github.com>
2024-01-09 20:50:23 +0330

    Subscribe BasketCheckout Queue Message Event in Ordering BasketCheckoutConsumer

---
+++ src/Services/Ordering/Ordering.API/EventBusConsumer/BasketCheckoutCnsumer.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using EventBus.Messages.Events;
+using MassTransit;
+using MediatR;
+using Ordering.Application.Features.Commands.CheckoutOrder;
+
+namespace Ordering.API.EventBusConsumer
+{
+    public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent>
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+        public BasketCheckoutConsumer(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            var checkoutOrderCommand = _mapper.Map<CheckoutOrderCommand>(context.Message);
+            var result = await _mediator.Send(checkoutOrderCommand);
+
+            _logger.LogInformation($"Basket checkout event consumed successfully. Created orderId:{result}");
+        }
+    }
+}
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,17 +1,39 @@
+using EventBus.Messages.Common;
+using MassTransit;
+using Ordering.API.EventBusConsumer;
 using Ordering.Application;
 using Ordering.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
+
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+//Mass-transit RabbitMQ Configuration
+builder.Services.AddMassTransit(configure => {
+    configure.AddConsumer<BasketCheckoutConsumer>();
+
+    configure.UsingRabbitMq((ctx, cnf) => {
+        cnf.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+        cnf.ReceiveEndpoint(EventBusConstans.BasketCheckoutQueue, c => {
+            c.ConfigureConsumer<BasketCheckoutConsumer>(ctx);
+        });
+    });
+});
+
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddScoped<BasketCheckoutConsumer>();
+
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
