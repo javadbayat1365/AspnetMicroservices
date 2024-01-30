ca6042e5ffc94f678f692f344bb71ab15ff63ab9
javad bayat <39430951+javadbayat1365@users.noreply.github.com>
2024-01-30 23:03:15 +0330

    add ocelot getway

--- src/ApiGetways/OcelotApiGW/Program.cs
+++ src/ApiGetways/OcelotApiGW/Program.cs
@@ -3,16 +3,20 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOcelot();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+
+builder.Configuration.AddJsonFile("ocelot.local.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
-builder.Host.ConfigureLogging((hostingContext, loggingBuilder) => {
-    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-   loggingBuilder.AddDebug();
-    loggingBuilder.AddConsole();
-});
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.MapGet("/", () => "Hello Ocelot");
 await app.UseOcelot();
+
 app.Run();
